ff1de2f450f929b07b82a13b367906ddf867346d
tarahcotta <msh5z8d6ps@privaterelay.appleid.com>
2026-09-18 22:50:43 -0600

    build: bump version to 23.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.aistudio.vitalstrength.wlongevity.nbiyda"
     minSdk = 24
     targetSdk = 36
-    versionCode = 22
-    versionName = "22.0"
+    versionCode = 23
+    versionName = "23.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
