61734c17c681297b4cd3314ea7761f5f66502de7
tarahcotta <msh5z8d6ps@privaterelay.appleid.com>
2026-09-21 18:23:31 -0600

    build: bump app version to 30.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.aistudio.vitalstrength.wlongevity.nbiyda"
     minSdk = 24
     targetSdk = 36
-    versionCode = 29
-    versionName = "29.0"
+    versionCode = 30
+    versionName = "30.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
