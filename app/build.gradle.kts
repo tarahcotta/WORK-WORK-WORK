0eca65509b76b9189d6eb5ce65c4ad0ada98655e
tarahcotta <msh5z8d6ps@privaterelay.appleid.com>
2026-09-22 21:46:53 -0600

    build: bump version to 38.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.aistudio.vitalstrength.wlongevity.nbiyda"
     minSdk = 24
     targetSdk = 36
-    versionCode = 37
-    versionName = "37.0"
+    versionCode = 38
+    versionName = "38.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
