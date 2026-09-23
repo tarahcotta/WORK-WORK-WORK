7ce8846408691f77b515daf4ac1b4809ef0b8c06
tarahcotta <msh5z8d6ps@privaterelay.appleid.com>
2026-09-22 21:30:31 -0600

    chore: bump version to 37.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.aistudio.vitalstrength.wlongevity.nbiyda"
     minSdk = 24
     targetSdk = 36
-    versionCode = 36
-    versionName = "36.0"
+    versionCode = 37
+    versionName = "37.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
